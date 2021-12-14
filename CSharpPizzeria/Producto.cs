776f5475ae29af24cc2bda00caf4f462c8688805
quiqueac <82471440+quiqueac@users.noreply.github.com>
2021-12-13 23:44:44 -0600

    Inicio del proyecto

---
+++ CSharpPizzeria/Producto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPizzeria.Clases
+{
+    public abstract class Producto
+    {
+        private string nombre;
+        private decimal precio;
+
+        public Producto(string nombre, decimal precio)
+        {
+            this.nombre = nombre;
+            this.precio = precio;
+        }
+
+        public abstract string Nombre();
+
+        public decimal Precio
+        {
+            get
+            {
+                return precio;
+            }
+            set
+            {
+                precio = value;
+            }
+        }
+    }
+}
